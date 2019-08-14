1e787dbf5d48fb39cf69d8ac080195a55e119f44
Kesara Wimal <kesarawimal9@gmail.com>
2019-08-14 23:05:28 +0530

    url validation issue fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -112,12 +112,7 @@ export default class Home extends React.Component {
     };
 
     validURL = (str) => {
-        let pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-            '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        let pattern = new RegExp('^(ftp|http|https):\/\/[^ "]+$');
         return !!pattern.test(str);
     }
 }
